289f648d2d8aceba32cf0c4dfb904c76b9fb5ed4
TLparche <h010508@naver.com>
2021-07-24 21:18:02 +0900

    asd

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
+'''import random
 
 read = open('제시어.txt', 'r', encoding='utf-8').read()
 list = []
@@ -10,4 +10,16 @@ for i in range(1, 7, 1):
     for i in range(3):
         data = list.pop(random.randrange(0, len(list)))
         print(data, end=' ')
+    print("입니다.")'''
+
+import random
+
+read = open('멘토.txt', 'r', encoding='utf-8').read()
+list = []
+list = read.split("\n")
+print(list)
+
+for i in range(1, 7, 1):
+    print(i, "조의 담당 멘토는 ", end=' ')
+    data = list.pop(random.randrange(0, len(list)))
     print("입니다.")
